2c60dc006b5f3f568697ed7add39c7d0040dd2a9
Swiichy <adlpromail@gmail.com>
2023-09-23 15:54:57 +0200

    refact: button

--- app/_components/ui/Button.tsx
+++ app/_components/ui/Button.tsx
@@ -12,6 +12,39 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   href?: string;
 }
 
+const getButtonClasses = (theme: "primary" | "secondary") => {
+  if (theme === "primary") {
+    return "text-white bg-purple hover:bg-purple-hover";
+  } else {
+    return "text-purple bg-transparent border border-purple hover:bg-purple-hover-secondary";
+  }
+};
+
+const getContent = (
+  text: string,
+  href: string | undefined,
+  iconUrl: string | undefined
+) => {
+  if (!href && iconUrl) {
+    return <Image src={iconUrl} width={20} height={20} alt="icon" />;
+  } else if (href && iconUrl) {
+    return (
+      <Link href={href} className="flex items-center justify-center">
+        <Image
+          className="md:hidden"
+          src={iconUrl}
+          width={20}
+          height={20}
+          alt="icon"
+        />
+        <span className="hidden md:block">{text}</span>
+      </Link>
+    );
+  } else {
+    return text;
+  }
+};
+
 export const Button = ({
   text,
   disabled,
@@ -20,46 +53,17 @@ export const Button = ({
   href,
   ...rest
 }: ButtonProps) => {
-  const handleTheme = () => {
-    if (theme === "primary") {
-      return "text-white bg-purple hover:bg-purple-hover";
-    } else {
-      return "text-purple bg-transparent border border-purple hover:bg-purple-hover-secondary";
-    }
-  };
-
-  const handleContent = () => {
-    if (!href && iconUrl) {
-      return <Image src={iconUrl} width={20} height={20} alt="icon" />;
-    } else if (href && iconUrl) {
-      return (
-        <Link href={href} className="flex items-center justify-center">
-          <Image
-            className="md:hidden"
-            src={iconUrl}
-            width={20}
-            height={20}
-            alt="icon"
-          />
-          <span className="hidden md:block">{text}</span>
-        </Link>
-      );
-    } else {
-      return text;
-    }
-  };
-
   return (
     <button
       className={clsx(
         "font-semibold rounded-lg transition py-3",
         disabled && "opacity-25 cursor-not-allowed",
         iconUrl ? "px-4" : "px-7",
-        handleTheme()
+        getButtonClasses(theme)
       )}
       {...rest}
     >
-      {handleContent()}
+      {getContent(text || "", href, iconUrl)}
     </button>
   );
 };
