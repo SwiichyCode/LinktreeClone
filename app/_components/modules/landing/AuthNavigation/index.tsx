17b387b7b8c45ff0b9fa0e72cbafb4fa9d4ead05
Swiichy <adlpromail@gmail.com>
2023-10-28 17:10:30 +0200

    UPDATE

--- app/_components/modules/landing/AuthNavigation/index.tsx
+++ app/_components/modules/landing/AuthNavigation/index.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import { useCookie } from "@/app/_hooks/useCookie";
 import { ButtonLink } from "@/app/_components/ui/ButtonLink";
 import { URL_CONSTANT } from "@/app/_constants/url.constant";
 
 export const AuthNavigation = () => {
+  const user = useCookie("user_id");
+
   return (
     <nav className="hidden md:flex justify-end min-w-logo-desktop">
-      <ButtonLink href={URL_CONSTANT.SIGN_IN} text="Login" variant="primary" />
+      {user ? (
+        <ButtonLink href={URL_CONSTANT.LINKS} text="Board" variant="primary" />
+      ) : (
+        <ButtonLink
+          href={URL_CONSTANT.SIGN_IN}
+          text="Login"
+          variant="primary"
+        />
+      )}
     </nav>
   );
 };
